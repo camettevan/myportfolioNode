3097d4e85c7ffcdb4aea1947e2f8836b6e49ab27
Camette <camettevan@gmail.com>
2019-11-14 15:52:27 -0800

    testing variables

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const webRoutes = require('./routes/web.js')
 const pug = require('pug')
 const path = require('path')
+require('dotenv').config()
 
 app.use(express.static('client/public'))
 app.set('view engine', 'pug')
@@ -11,5 +12,5 @@ app.set('views', path.join(__dirname, '../client/src/views'))
 app.use(webRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on http://localhost:3000/ to stop server press control + c')
+  console.log('Server is running on http://localhost:3000/ to stop server press control + c ' + process.env.DB_USERNAME)
 })
